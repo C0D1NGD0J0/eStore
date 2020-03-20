4f2e653a575cd2543447dd3e1d3b2d8695769caa
c0d1ngd0j0 <sam.gdouglas@gmail.com>
2020-03-20 17:44:53 -0400

    persist data in redux-store

--- src/store/index.js
+++ src/store/index.js
@@ -3,10 +3,11 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from "redux-thunk";
 import rootReducer from "../reducers";
 import logger from "redux-logger";
+import { persistStore } from "redux-persist";
 
 const initialState = {};
 const middlewares = [thunk];
 
-const store = createStore(rootReducer, initialState, composeWithDevTools(applyMiddleware(...middlewares)));
+export const store = createStore(rootReducer, initialState, composeWithDevTools(applyMiddleware(...middlewares)));
 
-export default store;
+export const persistor = persistStore(store);
